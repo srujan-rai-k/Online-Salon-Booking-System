68d372dc5e1b299f661fbfb4599094dce64c89cf
Srujan Rai K <63720536+srujan-rai-k@users.noreply.github.com>
2022-01-18 22:48:00 +0530

    Add files via upload

---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router=express.Router();
+
+const auth_controller=require('../controllers/auth');
+const auth=require('../middlewares/auth');
+
+
+router.get('/logout',auth.verify_loggedIn,auth_controller.logout);
+
+module.exports=router;
